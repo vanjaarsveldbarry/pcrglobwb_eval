c187297b1297e50981c7ac56cd950902cf9f0c0c
Barry van Jaaarsveld <a.s.vanjaarsveld@uu.nl>
2023-09-15 20:38:08 +0200

    small

--- grdcEval.py
+++ grdcEval.py
@@ -1,11 +1,6 @@
 from pcrglobwb_eval import preprocess
 from pcrglobwb_eval import grdc
 
-grdcRawDataDirectory='/scratch/depfg/7006713/data/validationData/GRDC/_GRDC_raw/data'
-saveFolder='/scratch/depfg/7006713/temp/test'
-preprocess.grdcData(grdcRawDataDirectory=grdcRawDataDirectory, saveFolder=saveFolder)
-
-
 grdcObservedDataDirectory='/scratch/depfg/7006713/data/validationData/GRDC'
 simDirectory = '/scratch/depfg/7006713/geowat_global_output/30sec/clone_1'
 savePath = '/scratch/depfg/7006713/temp/test/output'
