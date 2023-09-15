f8703043f98d3dbad68de0076cb9c10649422d71
Barry van Jaaarsveld <a.s.vanjaarsveld@uu.nl>
2023-09-15 20:14:58 +0200

    small

---
+++ grdcPre.py
@@ -0,0 +1,5 @@
+from pcrglobwb_eval import grdc
+
+grdcRawDataDirectory='/scratch/depfg/7006713/data/validationData/GRDC/_GRDC_raw/data'
+saveFolder='/scratch/depfg/7006713/temp/test'
+preprocess.grdcData(grdcRawDataDirectory=grdcRawDataDirectory, saveFolder=saveFolder)
