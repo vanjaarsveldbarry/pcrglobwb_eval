d0b1b53cf139e4c0e13e7a9ac94747801be4ed9e
Barry van Jaaarsveld <a.s.vanjaarsveld@uu.nl>
2023-09-29 14:07:53 +0200

    small

--- examples/grdc/1.grdcPre.py
+++ examples/grdc/1.grdcPre.py
@@ -1,12 +1,8 @@
 from pcrglobwb_eval import preprocess
 
 #Directory containing the GRDC raw data files(i.e. 1104150_Q_Day.Cmd.txt)
-grdcRawDataDirectory='/scratch/depfg/7006713/temp/test/raw'
-
+grdcRawDataDirectory='/scratch/depfg/7006713/data/validationData/GRDC/_GRDC_raw/data'
 
 #Directory where to save the preprocessed GRDC data:
 saveFolder='/scratch/depfg/7006713/temp/test'
-# - GRDC_array.zarr
-# - GRDC_points.shp
-
 preprocess.grdcData(grdcRawDataDirectory=grdcRawDataDirectory, saveFolder=saveFolder)
