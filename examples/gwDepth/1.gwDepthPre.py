e6c5fcb6e8afbaad8a10d840275f8bdd88972b8d
otoo0001 <otoo0001@gpu028.cluster>
2023-10-09 15:49:53 +0200

    git checkout n# Please enter the commit message for your changes. Lines starting t

--- examples/gwDepth/1.gwDepthPre.py
+++ examples/gwDepth/1.gwDepthPre.py
@@ -5,7 +5,7 @@ groundWaterDepthDataDirectory='/scratch/depfg/otoo0001/data/Groundwater_observat
 
 
 #Directory where to save the preprocessed GRDC data:
-saveFolder='/scratch/depfg/otoo0001/transient_analysis/gw_all/'
+saveFolder='/scratch/depfg/otoo0001/transient_analysis/gw_part/'
 # - GRDC_array.zarr
 # - GRDC_points.shp
 
